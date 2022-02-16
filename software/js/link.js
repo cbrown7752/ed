c0c0f3b7915ba9e43c2485eff49fbc8a273d5b37
culanndog3111 <59544410+cbrown7752@users.noreply.github.com>
2022-02-16 10:45:06 -0600

    Add files via upload

---
+++ software/js/link.js
@@ -0,0 +1,15 @@
+function AS(){
+    let val = 'https://echo-development.echobot.xyz/software#Alien-Shooter';
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    alert('Link Copyed');
+  }
