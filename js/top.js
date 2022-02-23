d82be256fe3276b252b87480f7285c24b9af9b5c
culanndog3111 <59544410+cbrown7752@users.noreply.github.com>
2022-02-23 16:27:24 -0600

    Update top.js

--- js/top.js
+++ js/top.js
@@ -5,6 +5,11 @@ var mybutton = document.getElementById("myBtn");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
+// Getting
+var theme = window.localStorage.getItem('theme')
+// Setting
+window.localStorage.setItem('theme', 'dark')
+
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
@@ -16,33 +21,10 @@ function scrollFunction() {
 function darkMode() {
    var element = document.body;
    element.classList.toggle("dark-mode");
+  // Getting
+var theme = window.localStorage.getItem('theme')
+// Setting
+window.localStorage.setItem('theme', 'dark')
 }
 
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-(function() {
-  let onpageLoad = localStorage.getItem("theme") || "";
-  let element = document.body;
-  element.classList.add(onpageLoad);
-  document.getElementById("theme").textContent =
-    localStorage.getItem("theme") || "light";
-})();
-
-function themeToggle() {
-  let element = document.body;
-  element.classList.toggle("dark-mode");
-
-  let theme = localStorage.getItem("theme");
-  if (theme && theme === "dark-mode") {
-    localStorage.setItem("theme", "");
-  } else {
-    localStorage.setItem("theme", "dark-mode");
-  }
-
-  document.getElementById("theme").textContent = localStorage.getItem("theme");
-}
