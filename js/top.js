f3acf6ef8ab6fbafa89c4b2c9c89a46d6e2e0fe1
culanndog3111 <59544410+cbrown7752@users.noreply.github.com>
2022-02-23 17:05:35 -0600

    Update top.js

--- js/top.js
+++ js/top.js
@@ -1,28 +1,4 @@
-
-//Get the button
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-
-    function myFunction() {
-       var element = document.body;
-       element.classList.toggle("dark-mode");
-    }
-
-
-    (function() {
+ (function() {
       let onpageLoad = localStorage.getItem("theme") || "";
       let element = document.body;
       element.classList.add(onpageLoad);
@@ -44,3 +20,20 @@ function scrollFunction() {
       document.getElementById("theme").textContent = localStorage.getItem("theme");
     }
     
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
