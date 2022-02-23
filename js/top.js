75a2296b64eea19b1e6773a00be023bb17b8a308
culanndog3111 <59544410+cbrown7752@users.noreply.github.com>
2022-02-23 17:17:59 -0600

    Update top.js

--- js/top.js
+++ js/top.js
@@ -1,27 +1,4 @@
- (function() {
-      let onpageLoad = localStorage.getItem("theme") || "";
-      let element = document.body;
-      element.classList.add(onpageLoad);
-      document.getElementById("theme").textContent =
-        localStorage.getItem("theme") || "light";
-    })();
-    
-    function themeToggle() {
-      let element = document.body;
-      element.classList.toggle("dark-mode");
-    
-      let theme = localStorage.getItem("theme");
-      if (theme && theme === "dark-mode") {
-        localStorage.setItem("theme", "");
-      } else {
-        localStorage.setItem("theme", "dark-mode");
-      }
-    
-      document.getElementById("theme").textContent = localStorage.getItem("theme");
-    }
-    
-
-
+ 
 //Get the button
 var mybutton = document.getElementById("myBtn");
 
