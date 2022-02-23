8c4dc0240704816f52d06cf12d00063b20092e37
culanndog3111 <59544410+cbrown7752@users.noreply.github.com>
2022-02-23 16:45:52 -0600

    Update top.js

--- js/top.js
+++ js/top.js
@@ -17,27 +17,9 @@ function scrollFunction() {
 
 
 
-// On page load set the theme.
-(function() {
-  let onpageLoad = localStorage.getItem("theme") || "";
-  let element = document.body;
-  element.classList.add(onpageLoad);
-  document.getElementById("theme").textContent =
-    localStorage.getItem("theme") || "light";
-})();
-
 function darkMode() {
-  let element = document.body;
-  element.classList.toggle("dark-mode");
-
-  let theme = localStorage.getItem("theme");
-  if (theme && theme === "dark-mode") {
-    localStorage.setItem("theme", "");
-  } else {
-    localStorage.setItem("theme", "dark-mode");
-  }
-
-  document.getElementById("theme").textContent = localStorage.getItem("theme");
+  const formElem = document.getElementById('dark-mode');
+  const formData = new FormData(formElem);
+  const darkModeState = !!formData.get('toggler');
+  localStorage.setItem('darkMode', darkModeState);
 }
-
-
