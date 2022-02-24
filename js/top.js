fab8e75065e934fd57b7e4bc698dc5d4a358922b
culanndog3111 <59544410+cbrown7752@users.noreply.github.com>
2022-02-23 21:36:53 -0600

    Update top.js

--- js/top.js
+++ js/top.js
@@ -1,11 +1,8 @@
- 
-//Get the button
+ //Get the button
 var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
-
-
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -15,7 +12,8 @@ function scrollFunction() {
   }
 }
 
-function darkMode() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
